eb382e3f61246956fe985dcc2f8f62ad36f9f059
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-16 16:32:15 +0530

    add leftBar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import LeftBar from "./components/leftBar/leftBar";
+import TopBar from "./components/topBar/topBar";
+import Gallery from "./components/Gallery/Gallery";
 
 function App() {
-
   return (
-    <>
-      Homepage
-    </>
-  )
+    <div className="app">
+      <LeftBar />
+      <div className="content">
+        <TopBar />
+        <Gallery />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
