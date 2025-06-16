c150b4c42d62804e9dffa6485fc93fc19b148f56
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-17 00:55:07 +0530

    add Routes

---
+++ Frontend/src/Routes/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomePage.css'
+import { Route, Routes } from 'react-router'
+import MainLayout from '../../Layouts/MainLayout'
+import Gallery from '../../components/Gallery/Gallery'
+
+const HomePage = () => {
+  return (
+    <Gallery/>
+  )
+}
+
+export default HomePage
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import HomePage from './Routes/HomePage/HomePage.jsx'
+import MainLayout from './Layouts/MainLayout.jsx'
+import Gallery from './components/Gallery/Gallery.jsx'
+import CreatePage from './Routes/CreatePage/CreatePage.jsx'
+import AuthPage from './Routes/AuthPage/AuthPage.jsx'
+import PostPage from './Routes/PostPage/PostPage.jsx'
+import SearchPage from './Routes/SearchPage/SearchPage.jsx'
+import ProfilePage from './Routes/ProfilePage/ProfilePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout/>}>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/create' element={<CreatePage/>} />
+        <Route path='/pins/:id' element={<PostPage/>} />
+        <Route path='/:username' element={<ProfilePage/>} />
+        <Route path='/search' element={<SearchPage/>} />
+        </Route>
+        <Route path='/auth' element={<AuthPage/>} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
