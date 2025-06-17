ca30c6576f1f77424b6adcd89e19ec562cc21a9b
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-17 21:32:46 +0530

    add folder name

--- Frontend/src/Layouts/MainLayout.jsx
+++ Frontend/src/Layouts/MainLayout.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './MainLayout.css'
 import {Outlet} from 'react-router'
 import LeftBar from '../components/LeftBar/LeftBar'
-import TopBar from '../components/temp/temp'
 
 const MainLayout = () => {
   return (
