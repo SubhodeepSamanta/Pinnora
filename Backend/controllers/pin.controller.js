d8d844d2fff45bf10278d4977984a4bc05dccb4d
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-19 03:05:21 +0530

    add Infinite scroll

--- Backend/controllers/pin.controller.js
+++ Backend/controllers/pin.controller.js
@@ -2,6 +2,9 @@ import Pin from "../models/pin.model.js"
 
 
 export const getPins= async (req,res)=>{
-    const pins= await Pin.find();
-    res.status(200).send(pins);
+    const pageNumber= Number(req.query.cursor) || 0;
+    const LIMIT=21;
+    const pins= await Pin.find().limit(LIMIT).skip(LIMIT*pageNumber);
+    const hasNextPage= pins.length===LIMIT;
+    res.status(200).send({pins, nextCursor: hasNextPage ? pageNumber+1 : null});
 }
--- Frontend/src/components/Gallery/Gallery.jsx
+++ Frontend/src/components/Gallery/Gallery.jsx
@@ -2,52 +2,34 @@ import React from 'react'
 import './Gallery.css'
 import GalleryItem from '../GalleryItem/GalleryItem';
 import axios from 'axios'
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const pins = [
-  { id: 1, media: '/pins/pin1.jpeg', height: 1000, width: 1260 },
-  { id: 2, media: '/pins/pin2.jpeg', height: 1200, width: 800 },
-  { id: 3, media: '/pins/pin3.jpeg', height: 1080, width: 720 },
-  { id: 4, media: '/pins/pin4.jpeg', height: 900, width: 600 },
-  { id: 5, media: '/pins/pin5.jpeg', height: 1024, width: 768 },
-  { id: 6, media: '/pins/pin6.jpeg', height: 1280, width: 720 },
-  { id: 7, media: '/pins/pin7.jpeg', height: 1920, width: 1080 },
-  { id: 8, media: '/pins/pin8.jpeg', height: 800, width: 600 },
-  { id: 9, media: '/pins/pin9.jpeg', height: 1600, width: 1200 },
-  { id: 10, media: '/pins/pin10.jpeg', height: 1400, width: 1260 },
-  { id: 11, media: '/pins/pin11.jpeg', height: 1400, width: 1260 },
-  { id: 12, media: '/pins/pin12.jpeg', height: 1260, width: 1260 },
-  { id: 13, media: '/pins/pin13.jpeg', height: 1890, width: 1260 },
-  { id: 14, media: '/pins/pin14.jpeg', height: 1260, width: 1260 },
-  { id: 15, media: '/pins/pin15.jpeg', height: 1024, width: 768 },
-  { id: 16, media: '/pins/pin16.jpeg', height: 800, width: 600 },
-  { id: 17, media: '/pins/pin17.jpeg', height: 1280, width: 720 },
-  { id: 18, media: '/pins/pin18.jpeg', height: 1920, width: 1080 },
-  { id: 19, media: '/pins/pin19.jpeg', height: 1600, width: 1200 },
-  { id: 20, media: '/pins/pin20.jpeg', height: 1400, width: 1260 },
-  { id: 21, media: '/pins/pin21.jpeg', height: 1080, width: 720 },
-  { id: 22, media: '/pins/pin22.jpeg', height: 900, width: 600 },
-  { id: 23, media: '/pins/pin23.jpeg', height: 1024, width: 768 },
-  { id: 24, media: '/pins/pin24.jpeg', height: 1280, width: 720 },
-  { id: 25, media: '/pins/pin25.jpeg', height: 1920, width: 1080 },
-];
-
-const fetchPins= async()=>{
-  const pins= await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/pins`);
+const fetchPins= async({pageParam})=>{
+  const pins= await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/pins?cursor=${pageParam}`);
   return pins.data;
 }
 
 const Gallery = () => {
-  const { isPending, error, data } = useQuery({ queryKey: ['pin'], queryFn: fetchPins });
-  if(error) return 'An error has occured: '+error.message;
-  if(isPending) return 'Loading...';
+  const { data, fetchNextPage, hasNextPage, status } = useInfiniteQuery(
+    { 
+      queryKey: ['pin'], 
+      queryFn: fetchPins,
+      initialPageParam: 0,
+      getNextPageParam: (lastPage)=> lastPage.nextCursor
+    });
+  if(status==='error') return 'Something went wrong...';
+  if(status==='pending') return 'Loading...';
+  const allPins= data.pages.flatMap((page)=>page.pins);
 
   return (
+      <InfiniteScroll dataLength={allPins.length} next={fetchNextPage} hasMore={!!hasNextPage} loader={<h4 className='scroll'> Loading more pins </h4>} endMessage={<h4 className='scroll'> All Pins Loaded </h4>} >
     <div className='gallery'>
-      {data.map((pin)=>(
+      {allPins.map((pin)=>(
         <GalleryItem key={pin._id} item={pin} />
       ))}
     </div>
+      </InfiniteScroll>
   )
 }
 
