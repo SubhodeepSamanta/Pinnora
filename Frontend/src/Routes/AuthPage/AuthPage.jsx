0bf0246965880e6cf1dd21ff15a6b5dce6be4a3b
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-18 17:41:45 +0530

    add authpage

--- Frontend/src/Routes/AuthPage/AuthPage.jsx
+++ Frontend/src/Routes/AuthPage/AuthPage.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Img } from "../../components/ImageKit/Image";
+import {Link} from 'react-router'
+import './AuthPage.css'
 
 const AuthPage = () => {
+  const [isRegister,setIsRegister]= useState(false);
   return (
-    <div>AuthPage</div>
-  )
+    <div className="authpage">
+      <div className="loginpage">
+        <Link to='/'><Img src="/general/logo.png" width="50" /></Link>
+        <h1>{isRegister?'Register your' : 'Login to your'} account</h1>
+        {isRegister ?
+        (<form action="" key='register-form'>
+          <div className="register-input">
+            <label htmlFor="name">
+              Name
+              <input type="name" name="name" id="name" placeholder="Name" />
+            </label>
+          </div>
+          <div className="register-input">
+            <label htmlFor="username">
+              Username
+              <input type="username" name="username" id="username" placeholder="Username" />
+            </label>
+          </div>
+          <div className="register-input">
+            <label htmlFor="email">
+              Email
+              <input type="email" name="email" id="email" placeholder="Email" />
+            </label>
+          </div>
+          <div className="register-input">
+            <label htmlFor="password">
+              Password
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </label>
+          </div>
+          <button className="auth-button">Register</button>
+          <span className="form-switch" onClick={()=>setIsRegister(i=>!i)} >Don&apos;t have an account? <b>Login</b></span>
+        </form>)
+        :
+        (<form action="" key='login-form'>
+          <div className="login-input">
+            <label htmlFor="email">
+              Email
+              <input type="email" name="email" id="email" placeholder="Email" />
+            </label>
+          </div>
+          <div className="login-input">
+            <label htmlFor="password">
+              Password
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </label>
+          </div>
+          <button className="auth-button">Login</button>
+          <span onClick={()=>setIsRegister(i=>!i)} className="form-switch" >Don&apos;t have an account? <b>Register</b></span>
+        </form>)
 }
+      </div>
+    </div>
+  );
+};
 
-export default AuthPage
+export default AuthPage;
