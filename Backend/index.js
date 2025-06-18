bd2188375c0c69db4cf00541bb2b863e76fa1f7f
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-18 18:52:54 +0530

    add backend routes and controllers

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import userRouter from './routes/user.route.js';
+import pinRouter from './routes/pin.route.js';
+import commentRouter from './routes/comment.route.js';
+import boardRouter from './routes/board.route.js';
+
+const app= express();
+const PORT=process.env.PORT;
+
+app.use('/users', userRouter );
+app.use('/pins', pinRouter );
+app.use('/comments', commentRouter );
+app.use('/boards', boardRouter );
+
+app.listen(PORT,()=>{ 
+    console.log(`Server is running on port: ${PORT}`);
+});
