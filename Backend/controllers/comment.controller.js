27d2795bea751c6401fafe979e90ac81ab99c868
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-19 22:07:35 +0530

    add comments backend fetch

--- Backend/controllers/comment.controller.js
+++ Backend/controllers/comment.controller.js
@@ -1,6 +1,9 @@
+import Comment from "../models/comment.model.js";
+import User from "../models/user.model.js";
 
 
-
-export const test= (req,res)=>{
-    res.json({"message":"response from comment"});
+export const getComments= async (req,res)=>{
+    const {postId}= req.params;
+    const comments= await Comment.find({pin: postId}).populate("user","username displayName img").sort({createdAt: -1});
+    res.status(200).send(comments);
 }
--- Backend/routes/comment.route.js
+++ Backend/routes/comment.route.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import { test } from '../controllers/comment.controller.js';
+import { getComments } from '../controllers/comment.controller.js';
 
 const router= express.Router();
 
-router.get('/',test);
+router.get('/:postId',getComments);
 
 export default router;
