cb5c4bf36e87d72353c28457cc5b139b9f27f7c6
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-22 22:23:11 +0530

    add final build

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,15 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
-import HomePage from "./Routes/HomePage/HomePage.jsx";
-import MainLayout from "./Layouts/MainLayout.jsx";
-import Gallery from "./components/Gallery/Gallery.jsx";
-import CreatePage from "./Routes/CreatePage/CreatePage.jsx";
-import AuthPage from "./Routes/AuthPage/AuthPage.jsx";
-import PostPage from "./Routes/PostPage/PostPage.jsx";
-import SearchPage from "./Routes/SearchPage/SearchPage.jsx";
-import ProfilePage from "./Routes/ProfilePage/ProfilePage.jsx";
+// import HomePage from "./Routes/HomePage/HomePage.jsx";
+// import MainLayout from "./Layouts/MainLayout.jsx";
+// import Gallery from "./components/Gallery/Gallery.jsx";
+// import CreatePage from "./Routes/CreatePage/CreatePage.jsx";
+// import AuthPage from "./Routes/AuthPage/AuthPage.jsx";
+// import PostPage from "./Routes/PostPage/PostPage.jsx";
+// import SearchPage from "./Routes/SearchPage/SearchPage.jsx";
+// import ProfilePage from "./Routes/ProfilePage/ProfilePage.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const HomePage= React.lazy(()=> import("./Routes/HomePage/HomePage.jsx"));
+const MainLayout= React.lazy(()=> import("./Layouts/MainLayout.jsx"));
+const CreatePage= React.lazy(()=> import("./Routes/CreatePage/CreatePage.jsx"));
+const AuthPage= React.lazy(()=> import("./Routes/AuthPage/AuthPage.jsx"));
+const PostPage= React.lazy(()=> import("./Routes/PostPage/PostPage.jsx"));
+const SearchPage= React.lazy(()=> import("./Routes/SearchPage/SearchPage.jsx"));
+const ProfilePage= React.lazy(()=> import("./Routes/ProfilePage/ProfilePage.jsx"));
 
 const queryClient = new QueryClient();
 
